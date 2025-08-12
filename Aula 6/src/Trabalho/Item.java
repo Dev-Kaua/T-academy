0b3b0104bd67ba353568da8a7763399530e4934d
assdasd <sdadas>
2025-08-12 11:55:59 -0300

    Aula 6 - trabalho de engenharia de automação

---
+++ Aula 6/src/Trabalho/Item.java
@@ -0,0 +1,11 @@
+package Trabalho;
+
+public class Item {
+    public String nome;
+    public double quantidade;
+
+    public Item(String nome, double quantidade) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+    }
+}
